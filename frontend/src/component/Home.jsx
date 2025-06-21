725e3633bfb62a9316f20baee5e656a244d2282a
samrth07 <smrthlad@gmail.com>
2025-06-21 22:40:24 +0530

    new chamges

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Card from './ui/Card'
 
 const Home = () => {
   return (
     <div>
             <Navbar/>
+            <Card/>
     </div>
   )
 }
---
+++ frontend/src/component/ui/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className=' h-64 w-2xl border-black  rounded-2xl text-center m-3  shadow-2xl bg-white'>
+            samarth
+    </div>
+  )
+}
+
+export default Card
