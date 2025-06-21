0d66640f88ec9a1db59197e7f8a8c2aa64c1fccc
shrutiii <shrutijadhav4102@gmail.com>
2025-06-22 04:29:34 +0530

    updated ws-routes

--- backend/http-backend/routeHandlers/chatHandler.js
+++ backend/http-backend/routeHandlers/chatHandler.js
@@ -3,6 +3,8 @@ import { client } from "../../../db/prisma";
 
 export const getMessages = async (req, res) => {
 
+    const roomId = req.params.roomId;
+    const userId = req.id;
     const messages = await client.message.findMany({
         where: {
             roomId: roomId
@@ -12,13 +14,52 @@ export const getMessages = async (req, res) => {
         },
         take: 100,
         include: {
-            sender: {
-                select: {
-                    id: true,
-                    name: true,
-                    profileImage: true
-                }
-            }
+            id: userId,
         }
     });
+
+    res.json({
+        messages: messages
+    })
+}
+
+export const connectPeople = async(req, res) => {
+    const userId = req.id;
+    const travelPlanId = req.body.travelPlanId;
+
+    let room = await client.connectionRoom.findFirst ({
+        where: {
+            travelPlanId,
+        }
+    })
+
+    if(!room) {
+        room = await client.connectionRoom.create ({
+            data: {
+                travelPlanId
+            }
+        })
+    }
+
+    const existingMember = await client.connectionMember.findFirst ({
+        where: {
+            userId: userId,
+            roomId: room.id
+        }
+    })
+
+    if(!existingMember) {
+        await client.connectionMember.create({
+            data: {
+                userId,
+                roomId: room.id
+            }
+        })
+    }
+
+    res.status(200).json ({
+        roomId: room.id,
+        message: "Connected to travel plan successfully!"
+    })
+    return;
 }
