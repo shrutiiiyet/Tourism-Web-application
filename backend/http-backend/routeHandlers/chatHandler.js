054bac7cef042b05ef5e1361a55293864f40af36
shrutiii <shrutijadhav4102@gmail.com>
2025-06-22 04:35:32 +0530

    added try-catch blocks

--- backend/http-backend/routeHandlers/chatHandler.js
+++ backend/http-backend/routeHandlers/chatHandler.js
@@ -3,63 +3,79 @@ import { client } from "../../../db/prisma";
 
 export const getMessages = async (req, res) => {
 
-    const roomId = req.params.roomId;
-    const userId = req.id;
-    const messages = await client.message.findMany({
-        where: {
-            roomId: roomId
-        },
-        orderBy: {
-            createdAt: 'desc'
-        },
-        take: 100,
-        include: {
-            id: userId,
-        }
-    });
+    try {
+        const roomId = req.params.roomId;
+        const userId = req.id;
+        const messages = await client.message.findMany({
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            take: 100,
+            include: {
+                id: userId,
+            }
+        });
 
-    res.json({
-        messages: messages
-    })
+        res.json({
+            messages: messages
+        })
+    }
+    catch(e) {
+        res.status(500).json ({
+            message: "error fetching messages",
+            error: e
+        })
+    }
 }
 
 export const connectPeople = async(req, res) => {
-    const userId = req.id;
-    const travelPlanId = req.body.travelPlanId;
 
-    let room = await client.connectionRoom.findFirst ({
-        where: {
-            travelPlanId,
-        }
-    })
+    try {
+        const userId = req.id;
+        const travelPlanId = req.body.travelPlanId;
 
-    if(!room) {
-        room = await client.connectionRoom.create ({
-            data: {
-                travelPlanId
+        let room = await client.connectionRoom.findFirst ({
+            where: {
+                travelPlanId,
             }
         })
-    }
 
-    const existingMember = await client.connectionMember.findFirst ({
-        where: {
-            userId: userId,
-            roomId: room.id
+        if(!room) {
+            room = await client.connectionRoom.create ({
+                data: {
+                    travelPlanId
+                }
+            })
         }
-    })
 
-    if(!existingMember) {
-        await client.connectionMember.create({
-            data: {
-                userId,
+        const existingMember = await client.connectionMember.findFirst ({
+            where: {
+                userId: userId,
                 roomId: room.id
             }
         })
-    }
 
-    res.status(200).json ({
-        roomId: room.id,
-        message: "Connected to travel plan successfully!"
-    })
-    return;
+        if(!existingMember) {
+            await client.connectionMember.create({
+                data: {
+                    userId,
+                    roomId: room.id
+                }
+            })
+        }
+
+        res.status(200).json ({
+            roomId: room.id,
+            message: "Connected to travel plan successfully!"
+        })
+    }
+    catch(e) {
+        res.status(500).json({
+            message: "Something went wrong!",
+            error: e
+        })
+    }
 }
