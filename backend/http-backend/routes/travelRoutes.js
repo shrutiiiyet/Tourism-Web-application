59b8d52ae3b6e3a2d5cf53995c43b0dce88a32b8
shrutiii <shrutijadhav4102@gmail.com>
2025-06-26 23:18:29 +0530

    added routes

--- backend/http-backend/routes/travelRoutes.js
+++ backend/http-backend/routes/travelRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import getTravelPlans from '../routeHandlers/travelHandler';
+import { middleware } from './middleware';
+import { CreateRoom, joinRoom, leaveRoom } from '../routeHandlers/roomHandler.js';
 
 const travelRouter = express.Router();
 
 travelRouter.get('/:filter', getTravelPlans);
+
+travelRouter.use(middleware);
+
+userRouter.post('/create', CreateRoom);
+userRouter.post('/join', joinRoom);
+userRouter.post('/leave', leaveRoom);
 
 export default travelRouter
