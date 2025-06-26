c54c7e5ce742bddf98d7d3b53fcc366af448535d
shrutiii <shrutijadhav4102@gmail.com>
2025-06-27 00:13:17 +0530

    replaced middleware

--- backend/http-backend/routes/travelRoutes.js
+++ backend/http-backend/routes/travelRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getTravelPlans } from '../routeHandlers/travelHandler.js';
-import { middleware } from './middleware.js';
+import { middleware } from '../utils/middleware.js';
 import { CreateRoom, joinRoom, leaveRoom } from '../routeHandlers/roomHandler.js';
 
 const travelRouter = express.Router();
